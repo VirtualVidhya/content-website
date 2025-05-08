3459bd18e6a2f2c06c9b6d5d73ef57fa506d27d5
kharva <parvadesai404@gmail.com>
2025-05-08 15:38:34 +0530

    [Update] operators & var ex. (py)

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -7,13 +7,14 @@ import { SITE } from "@/config";
 export async function GET() {
   const posts = await getCollection("blog");
   const sortedPosts = getSortedPosts(posts);
+
   return rss({
     title: SITE.title,
     description: SITE.desc,
     site: SITE.website,
     items: sortedPosts.map(({ data, id, filePath }) => ({
-      link: getPath(id, filePath),
       title: data.title,
+      link: getPath(id, filePath),
       description: data.description,
       pubDate: new Date(data.modDatetime ?? data.pubDatetime),
     })),
